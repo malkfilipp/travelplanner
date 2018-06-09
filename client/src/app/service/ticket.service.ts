d5e296c8cd7ab660460af563b2a548303da809f8
malkfilipp <malkfillipp@gmail.com>
2018-06-09 20:20:20 +0300

    Set ticketApi as an environment variable

--- client/src/app/service/ticket.service.ts
+++ client/src/app/service/ticket.service.ts
@@ -2,18 +2,15 @@ import {Injectable} from '@angular/core';
 import {HttpClient, HttpParams} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {Ticket} from '../model/ticket';
+import {environment} from '../../environments/environment';
 
 @Injectable()
 export class TicketService {
-  // TODO: move to config file
-  public API = '//localhost:8080';
-  public TICKET_API = this.API + '/tickets';
-
   constructor(private http: HttpClient) {
   }
 
   get(departure: string, arrival: string, date: string): Observable<Ticket[]> {
-    return this.http.get<Ticket[]>(this.TICKET_API, {
+    return this.http.get<Ticket[]>(environment.ticketsApi, {
       params: new HttpParams()
         .set('departure', departure)
         .set('arrival', arrival)
--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  ticketsApi: 'http://localhost:8080/tickets'
 };
